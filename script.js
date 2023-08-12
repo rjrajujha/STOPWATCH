7a63c035e2b4b9cefad1743883518cf27054a67d
rjrajujha <rjrajujha@outlook.com>
2023-08-12 09:19:29 +0530

    update

--- script.js
+++ script.js
@@ -1,91 +1,48 @@
-//Current Time
-let currentTime = document.getElementById("currenttime");
+let startTime = 0;
+let running = false;
+let interval;
 
-//Buttons
-let startBtn = document.getElementById("start_btn");
-let pauseBtn = document.getElementById("pause_btn");
-let resetBtn = document.getElementById("reset_btn");
-let castBtn = document.getElementById("cast_btn");
+const display = document.getElementById("display");
+const startStopButton = document.getElementById("startStop");
+const resetButton = document.getElementById("reset");
 
-// Casts
-let casts = document.getElementById("casts");
-let castCount = 1;
+startStopButton.addEventListener("click", toggleStartStop);
+resetButton.addEventListener("click", reset);
 
-
-let StopWatch = (command) => {
-
-    //Time
-    let time_ms = parseInt(currentTime.innerText.split(":")[3]);
-    let time_sec = parseInt(currentTime.innerText.split(":")[2]);
-    let time_min = parseInt(currentTime.innerText.split(":")[1]);
-    let time_hour = parseInt(currentTime.innerText.split(":")[0]);
-
-    let startTimer = () => {
-        time_ms = time_ms + 1;
-
-        if (time_ms === 1000) {
-            time_ms = 0;
-            time_sec = time_sec + 1;
-        }
-
-        if (time_sec === 60) {
-            time_sec = 0;
-            time_min = time_min + 1;
-        }
-
-        if (time_min === 60) {
-            time_min = 0;
-            time_hour = time_hour + 1;
-        }
-        currentTime.innerText = `${time_hour}:${time_min}:${time_sec}:${time_ms}`
+function toggleStartStop() {
+    if (!running) {
+        startStopButton.textContent = "Stop";
+        running = true;
+        startTime = Date.now() - (startTime > 0 ? startTime : 0);
+        interval = setInterval(updateDisplay, 10);
+    } else {
+        startStopButton.textContent = "Start";
+        running = false;
+        clearInterval(interval);
     }
-
-    function onCommand(cmd) {
-        let myInterval = setInterval(startTimer, 1);
-        if (cmd == "pause" || cmd == "reset") {
-            clearInterval(myInterval);
-        }
-    }
-
-    if (command === "start") {
-        onCommand()
-        console.log("Stopwatch Started");
-    }
-
-    if (command === "pause") {
-        onCommand("pause")
-        console.log("Stopwatch Paused")
-    }
-
-    if (command === "reset") {
-        currentTime.innerText = `00:00:00:000`
-        onCommand("reset")
-        console.log("Stopwatch Reseted");
-    }
-
 }
 
+function updateDisplay() {
+    const elapsed = Date.now() - startTime;
+    const time = new Date(elapsed);
+    const minutes = time.getUTCMinutes();
+    const seconds = time.getUTCSeconds();
+    const milliseconds = time.getUTCMilliseconds();
+    display.textContent = `${formatTime(minutes)}:${formatTime(seconds)}:${formatTime(milliseconds, true)}`;
+}
 
-startBtn.addEventListener("click", () => {
-    StopWatch("start");
-});
+function reset() {
+    clearInterval(interval);
+    running = false;
+    startStopButton.textContent = "Start";
+    startTime = 0;
+    display.textContent = "00:00:000";
+}
 
-pauseBtn.addEventListener("click", () => {
-    StopWatch("pause");
-})
-
-resetBtn.addEventListener("click", () => {
-    StopWatch("reset");
-})
-
-castBtn.addEventListener("click", () => {
-
-    const para = document.createElement("p");
-    const node = document.createTextNode(`CAST ${castCount} : ${currentTime.innerText}`);
-
-    para.appendChild(node);
-    document.getElementById("casts").appendChild(para);
-
-    console.log("Cast Appended");
-    castCount++;
-})
+function formatTime(value, isMilliseconds = false) {
+    if (isMilliseconds) {
+        return value.toString().padStart(3, "0");
+    } else {
+        return value.toString().padStart(2, "0");
+    }
+}
